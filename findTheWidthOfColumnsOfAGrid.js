1238cd46daeff4c58732c5556eaad706993c52c8
Claudia  P. R. Soto <claudiarojassoto@gmail.com>
2023-11-26 16:49:26 -0300

    Update findTheWidthOfColumnsOfAGrid.js
    Delete comments

--- findTheWidthOfColumnsOfAGrid.js
+++ findTheWidthOfColumnsOfAGrid.js
@@ -1,17 +1,14 @@
 var findColumnWidth = function(grid) {
-    const m = grid.length;    // Number of rows
-    const n = grid[0].length; // Number of columns
-    let maxWidths = new Array(n).fill(0); // Initialize widths array
+    const m = grid.length;
+    const n = grid[0].length;
+    let maxWidths = new Array(n).fill(0);
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            // Calculate length of the integer considering negative sign
             let length = Math.abs(grid[i][j]).toString().length;
             if (grid[i][j] < 0) {
-                length++; // Account for negative sign
+                length++;
             }
-
-            // Update the maximum width for the column
             maxWidths[j] = Math.max(maxWidths[j], length);
         }
     }

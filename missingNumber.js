46f21a81eb52710be328f69e3b22380f9d1ef6ff
Claudia  P. R. Soto <claudiarojassoto@gmail.com>
2023-12-06 23:19:46 -0300

    Create missingNumber.js
    Add a solution to missing number challenge from leet code

---
+++ missingNumber.js
@@ -0,0 +1,11 @@
+var missingNumber = function (nums) {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    actualSum = actualSum + nums[i]
+  }
+  
+  return expectedSum-actualSum
+};
